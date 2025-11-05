95df673b83dd5c8ce36df3af7e014fe2f65ec47d
Rajpatel2510 <patelraj36900@gmail.com>
2025-11-05 16:18:47 +0530

    changes in embedder.py

--- embedder.py
+++ embedder.py
@@ -4,7 +4,8 @@ from typing import List
 class Embedder:
     def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
         try:
-            self.model = SentenceTransformer(model_name)
+            # self.model = SentenceTransformer(model_name)
+            self.model = SentenceTransformer(model_name, device="cpu")
             # print(f"Model loaded: {model_name}")
 
         except Exception as error:
